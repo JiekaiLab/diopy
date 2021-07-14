4fa15db6494d794dcb2beb0782cadf2ed421cd18
fenghuijian <fenghuijian@outlook.com>
2021-07-14 16:58:06 +0800

    fix reading rds function

--- diopy/diopyR.R
+++ diopy/diopyR.R
@@ -8,16 +8,10 @@ spec <- matrix(
 )
 opt <- getopt(spec)
 # read the rds file
-# data <- readRDS(getopt$readfile)
+data <- readRDS(getopt$readfile)
 ind <- gregexpr('/', opt$readfile)
 rdir <- substr(opt$readfile, start = 1, stop = max(ind[[1]]))
 fname <- substr(opt$readfile, start = max(ind[[1]])+1, stop = nchar(opt$readfile))
 wname <- gsub('.rds', '_tmp.h5', fname)
-# write_h5(data = data, object.type = getopt$targetobject ,file = paste0(rdir, wname),
-#          assay.name = "RNA", save.graphs = TRUE, save.scale = TRUE)
-print(opt$readfile)
-print(opt$targetobject)
-print(ind)
-print(rdir)
-print(fname)
-print(wname)
+write_h5(data = data, object.type = getopt$targetobject ,file = paste0(rdir, wname),
+         assay.name = "RNA", save.graphs = TRUE, save.scale = TRUE)
